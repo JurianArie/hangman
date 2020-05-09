62483ce8ebbac44eb9cd019c0cd4f59198cbffef
JurianArie <28654085+JurianArie@users.noreply.github.com>
2020-05-09 15:58:53 +0200

    Add ClassicProgressStrategy

---
+++ src/ProgressStrategies/ClassicProgressStrategy.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from src.ProgressStrategies.AbstractProgressStrategy import AbstractProgressStrategy
+from src.ProgressStrategies.WordRevealer import WordRevealer
+
+
+class ClassicProgressStrategy(AbstractProgressStrategy):
+    def __init__(self):
+        super().__init__(7)
+
+    def draw_progress(self, word: str, good_guesses: List[str]) -> str:
+        progress = self.get_hang_mans()[self.triesLeft]
+
+        return progress + '\n' + WordRevealer.reveal(word, good_guesses)
+
+    # The classic stages keyed by tries left.
+    def get_hang_mans(self):
+        return {6: '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', 5: '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', 4: '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', 3: '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', 2: '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', 1: '''+---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', 0: '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    ========='''}
---
+++ tests/test_ClassicProgressStrategy.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from src.ProgressStrategies.ClassicProgressStrategy import ClassicProgressStrategy
+
+
+class TestClassicProgressStrategy(TestCase):
+    def it_starts_with_seven_tries(self):
+        self.assertEqual(7, ClassicProgressStrategy().get_tries_left())
+
+    def test_draw_progress(self):
+        classic = ClassicProgressStrategy()
+        classic.decrease_tries_left()
+        self.assertEqual(classic.draw_progress('word', list('wr')), classic.get_hang_mans()[6] + '\nw_r_')
+        classic.decrease_tries_left()
+        classic.decrease_tries_left()
+        self.assertEqual(classic.draw_progress('word', list('wr')), classic.get_hang_mans()[4] + '\nw_r_')
