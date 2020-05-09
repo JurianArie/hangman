90e75f8dbebb7e5cde00af543b702119cd9d0ae1
JurianArie <28654085+JurianArie@users.noreply.github.com>
2020-05-09 15:44:14 +0200

    Add tests for SimpleGuessingStrategy

---
+++ tests/test_SimpleGuessingStrategy.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from src.GuessingStrategies.SimpleGuessingStrategy import SimpleGuessingStrategy
+
+
+class TestSimpleGuessingStrategy(TestCase):
+    def test_guess_is_correct(self):
+        guesser = SimpleGuessingStrategy('python')
+        self.assertTrue(guesser.guess_is_correct('python'))
+        self.assertTrue(guesser.guessed_the_word())
+        self.assertFalse(guesser.all_letters_have_been_guessed())
+
+        self.assertFalse(guesser.guess_is_correct('blabla'))
+        self.assertFalse(guesser.guess_is_correct(''))
+        self.assertFalse(guesser.guessed_the_word())
+
+    def test_it_is_case_insensitive(self):
+        guesser = SimpleGuessingStrategy('Word')
+        self.assertEquals('word', guesser.get_word())
+        self.assertTrue(guesser.guess_is_correct('wOrD'))
+        self.assertTrue(guesser.guessed_the_word())
+
+    def test_it_checks_previous_tries(self):
+        guesser = SimpleGuessingStrategy('impossible')
+        self.assertFalse(guesser.guess_is_correct('python'))
+        self.assertFalse(guesser.guess_is_correct('python'))
+        self.assertTrue(guesser.guess_has_been_tried('pytHon'))
+
+    def test_it_will_check_letters(self):
+        guesser = SimpleGuessingStrategy('hang')
+        self.assertFalse(guesser.guess_is_correct('h'))
+        self.assertFalse(guesser.guess_is_correct('a'))
+        self.assertFalse(guesser.guess_is_correct('n'))
+        self.assertEquals(list('han'), guesser.get_good_guesses())
+
+        self.assertTrue(guesser.guess_is_correct('g'))
+        self.assertTrue(guesser.all_letters_have_been_guessed())
