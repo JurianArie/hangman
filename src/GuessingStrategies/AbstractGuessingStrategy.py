863eca1088d3256a0d8ac787b9066a2cd0ed8ba9
JurianArie <28654085+JurianArie@users.noreply.github.com>
2020-05-09 14:12:47 +0200

    Add SimpleGuessingStrategy

---
+++ src/GuessingStrategies/AbstractGuessingStrategy.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class AbstractGuessingStrategy(ABC):
+    word: str
+    guessedCorrectly: bool = False
+    correctlyGuessedLetters: List[str] = []
+    guesses: List[str] = []
+
+    def __init__(self, word: str) -> None:
+        self.word = word
+
+    @abstractmethod
+    def guess_is_correct(self, guess: str) -> bool:
+        pass
+
+    def guess_has_been_tried(self, guess: str) -> bool:
+        return guess in self.guesses
+
+    def all_letters_have_been_guessed(self) -> bool:
+        unique_letters = list(set(self.word))
+        guessed = len(unique_letters) == len(self.correctlyGuessedLetters)
+
+        return self.set_guessed_correctly(guessed)
+
+    def get_word(self) -> str:
+        return self.word
+
+    def get_good_guesses(self) -> List[str]:
+        return self.correctlyGuessedLetters
+
+    def guessed_the_word(self) -> bool:
+        return self.guessedCorrectly
+
+    def set_guessed_correctly(self, guessed: bool) -> bool:
+        self.guessedCorrectly = guessed
+
+        return guessed
---
+++ src/GuessingStrategies/SimpleGuessingStrategy.py
@@ -0,0 +1,19 @@
+from src.GuessingStrategies.AbstractGuessingStrategy import AbstractGuessingStrategy
+
+
+class SimpleGuessingStrategy(AbstractGuessingStrategy):
+    def __init__(self, word: str):
+        super().__init__(word)
+
+    def guess_is_correct(self, guess: str) -> bool:
+        if self.guess_has_been_tried(guess):
+            return False
+
+        self.guesses.append(guess)
+
+        if len(guess) == 1 and guess in self.word:
+            self.correctlyGuessedLetters.append(guess)
+        elif guess == self.word:
+            return self.set_guessed_correctly(True)
+
+        return self.all_letters_have_been_guessed()
