c816f2727875ae31f844d91790b67d9f9e7595eb
JurianArie <28654085+JurianArie@users.noreply.github.com>
2020-05-09 14:15:21 +0200

    Add script for playing hang man

---
+++ main.py
@@ -0,0 +1,14 @@
+from os import system
+
+from src.GuessingStrategies.SimpleGuessingStrategy import SimpleGuessingStrategy
+from src.HangMan import HangMan
+from src.ProgressStrategies.SimpleProgressStrategy import SimpleProgressStrategy
+
+word = input('Enter a word\n')
+
+system('clear')
+
+progress_strategy = SimpleProgressStrategy(9)
+guessing_strategy = SimpleGuessingStrategy(word)
+
+HangMan(guessing_strategy, progress_strategy).play()
