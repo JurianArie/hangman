3a2d8d5ef9482c91ff9882de8148c1f01c0eae2f
JurianArie <28654085+JurianArie@users.noreply.github.com>
2020-05-10 16:21:04 +0200

    Add random guessing mode in setup

--- src/Preferences.py
+++ src/Preferences.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from src.GuessingStrategies.ClassicGuessingStrategy import ClassicGuessingStrategy
 from src.GuessingStrategies.GuessingInterfaceStrategy import GuessingInterfaceStrategy
 from src.GuessingStrategies.GuessingStrategyFactory import GuessingStrategyFactory
@@ -23,7 +25,7 @@ class Preferences:
         progress_option = input('Enter mode:\n')
 
         if progress_option not in progress_options:
-            # Default to the simple mode.
+            # Default to the first mode.
             progress_option = '0'
 
         return progress_options[progress_option]
@@ -39,22 +41,36 @@ class Preferences:
             '5': GuessingStrategyFactory.words_only_with_hints(''),
             '6': GuessingStrategyFactory.case_sensitive_classic_with_hints(''),
             '7': GuessingStrategyFactory.case_sensitive_words_only_with_hints(''),
+            '8': 'random',
+        }
+
+        guessing_option_names = {
+            '0': 'classic',
+            '1': 'words only',
+            '2': 'case sensitive classic',
+            '3': 'case sensitive words only',
+            '4': 'classic with hints',
+            '5': 'words only with hints',
+            '6': 'case sensitive classic with hints',
+            '7': 'case sensitive words only with hints',
+            '8': 'random',
         }
 
         print('Which guessing mode would you like?')
-        print('0: classic')
-        print('1: words only')
-        print('2: case sensitive classic')
-        print('3: case sensitive words only')
-        print('4: classic with hints')
-        print('5: words only with hints')
-        print('6: case sensitive classic with hints')
-        print('7: case sensitive words only with hints')
+        for i in range(len(guessing_option_names)):
+            i = str(i)
+            print(i + ': ' + guessing_option_names[str(i)])
 
         guessing_option = input('Enter mode:\n')
 
         if guessing_option not in guessing_options:
-            # Default to the simple mode.
+            # Default to the first mode.
             guessing_option = '0'
+
+        if guessing_option_names[guessing_option] == 'random':
+            random_choice = str(randrange(0, len(guessing_options) - 1))
+
+            guessing_option = random_choice
+            print('The random mode is: %s' % guessing_option_names[random_choice])
 
         return guessing_options[guessing_option]
